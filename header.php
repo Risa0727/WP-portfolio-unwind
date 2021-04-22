20a53e873b2538ed40456a56d59ef468553e305f
risarisarisa <from.aqua.to@gmail.com>
2021-04-22 22:11:58 +1000

    The js function has been moved to custom.js

--- header.php
+++ header.php
@@ -22,26 +22,7 @@
 <?php wp_head(); ?>
 
 <script>
- // ==================================
-// Navigation gets smaller when scroll down
-//  ==================================
-jQuery( function( $ ) {
-	var header = $('#masthead .sticky-bar');
-	// var logo = $('.site-branding');
-	// var navi = $('.site-navigation');
-  $scroll = $(window).scrollTop();
-	var headerH = header.outerHeight(true);
 
-	$(window).on('scroll', function() {
-		  $scroll = $(window).scrollTop();
-			if ($scroll >= headerH) {
-				header.addClass('is-scroll');
-			} else if ($scroll <= headerH) {
-				header.removeClass('is-scroll');
-			}
-	});
-
-});
 </script>
 
 </head>
--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,21 @@
 jQuery( function( $ ) {
   // console.log('hoge');
+  // ==================================
+ // Navigation gets smaller when scroll down
+ //  ==================================
+   var header = $('#masthead .sticky-bar');
+   // var logo = $('.site-branding');
+   // var navi = $('.site-navigation');
+   $scroll = $(window).scrollTop();
+   var headerH = header.outerHeight(true);
+
+   $(window).on('scroll', function() {
+       $scroll = $(window).scrollTop();
+       if ($scroll >= headerH) {
+         header.addClass('is-scroll');
+       } else if ($scroll <= headerH) {
+         header.removeClass('is-scroll');
+       }
+   });
 
 });
