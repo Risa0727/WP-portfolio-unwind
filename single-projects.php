60a1d2f45d9a167c954baa910210b31c19b623f3
risarisarisa <from.aqua.to@gmail.com>
2021-07-13 01:18:05 +1000

    Adjusted the style

--- single-projects.php
+++ single-projects.php
@@ -22,36 +22,37 @@ get_header(); ?>
 					<time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('d, m Y'); ?></time>
 				</div> -->
 				<div class="content-wrapper">
-					<div class="-project-image"><img src="<?php the_field('image'); ?>" alt="" /></div>
-					<div class="post-content">
-						<h2 class="sub-title title-h2">Project Brief</h2>
-						<?php the_content(); ?>
+					<div class="content-left">
+						<div class="-project-image"><img src="<?php the_field('image'); ?>" alt="" /></div>
 					</div>
-					<div class="project-tec"><?php echo get_the_term_list($post->ID,'project_tag','',' | '); ?></div>
-					<?php
-						// Load field value.
-						$dateStr = get_field('year');
-						$date = new DateTime();
-						$date = DateTime::createFromFormat('d/m/Y', $dateStr);
-						// show only Month and Year
-						// echo $date->format('M Y');
-					?>
-					<div class="project-year">
-						<h3 class="sub-title title-h3">Year</h3>
-						<?php echo $date->format('M Y'); ?>
-					</div>
-					<div class="project-btn">
-						<a href="<?php the_field('project_url'); ?>">View Website</a>
-					</div>
-				</div>
+					<div class="content-right">
+						<div class="post-content">
+							<h2 class="sub-title title-h2">Project Brief</h2>
+							<?php the_content(); ?>
+						</div>
+						<div class="project-tec"><?php echo get_the_term_list($post->ID,'project_tag','',' | '); ?></div>
+						<?php
+							// Load field value.
+							$dateStr = get_field('year');
+							$date = new DateTime();
+							$date = DateTime::createFromFormat('d/m/Y', $dateStr);
+							// show only Month and Year
+							// echo $date->format('M Y');
+						?>
+						<div class="project-year">
+							<h3 class="sub-title title-h3">Year</h3>
+							<p><?php echo $date->format('M Y'); ?></p>
+						</div>
+						<div class="project-btn">
+							<a href="<?php the_field('project_url'); ?>" target="_blank">View Website</a>
+						</div>
+					</div><!-- .content-left -->
+				</div><!-- .content-wrapper -->
 			<?php endwhile; endif; ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 </div><!-- .project-wrapper -->
-
-
-
 
 <?php
 // get_sidebar();
